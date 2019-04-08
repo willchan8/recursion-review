1a235d165ce9885fd1554b2028c98168adeeb593
willchan8 <wchan1992@gmail.com>
2019-04-08 12:29:26 -0700

    Re-implemented recursive stringifyJSON up to objects

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,63 @@
 
 // but you don't so you're going to write it from scratch:
 
+// var stringifyJSON = function(obj) {
+//   // your code goes here
+//   if (Array.isArray(obj)) {
+//     if (obj.length !== 0) {
+//       if (typeof obj[0] === "string") {
+//         return '[' + '"' + obj[0] + '"' + ']';
+//       } else {
+//         return '[' + obj[0] + ']';
+//       }
+//       // var results = [];
+
+//       // return '[' + results.join(',') + ']';
+
+//     } else {
+//       return '[]';
+//     }
+//   }
+  
+//   if (typeof obj === "string") {
+//     return '"' + obj + '"';
+//   } else {
+//     return '' + obj;
+//   }
+  
+//   return results;
+  
+// };
+
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (Array.isArray(obj)) {
+    if (obj.length !== 0) {
+      // for loop (var i etc. etc.)
+      var results = [];
+      for (var i = 0; i < obj.length; i++) {
+        results.push(stringifyJSON(obj[i]));
+      }
+
+    return '[' + results.join(',') + ']';
+
+    } else {
+      return '[]';
+    }
+  }
+  
+  if (typeof obj === "string") {
+    return '"' + obj + '"';
+  } else {
+    return '' + obj;
+  }
+  
+    if (Object.keys(obj).length === 0) {
+    return '{}';
+  }
+  
 };
+
+// results.push(stringifyJSON(obj[i]));
+
+// return '[' + results.join(',') + ']';
