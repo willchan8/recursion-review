302f68750b4a5420b541c335aae3fcec59c98c8a
willchan8 <wchan1992@gmail.com>
2019-04-08 14:57:31 -0700

    Re-implement to pass all tests

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,38 +1,33 @@
-// this is what you would do if you liked things to be easy:
-// var stringifyJSON = JSON.stringify;
-
-// but you don't so you're going to write it from scratch:
-
-// var stringifyJSON = function(obj) {
-//   // your code goes here
-//   if (Array.isArray(obj)) {
-//     if (obj.length !== 0) {
-//       if (typeof obj[0] === "string") {
-//         return '[' + '"' + obj[0] + '"' + ']';
-//       } else {
-//         return '[' + obj[0] + ']';
-//       }
-//       // var results = [];
-
-//       // return '[' + results.join(',') + ']';
-
-//     } else {
-//       return '[]';
-//     }
-//   }
-  
-//   if (typeof obj === "string") {
-//     return '"' + obj + '"';
-//   } else {
-//     return '' + obj;
-//   }
-  
-//   return results;
-  
-// };
-
 var stringifyJSON = function(obj) {
   // your code goes here
+  
+  if (obj === null) {
+    return 'null';
+  }
+  
+  
+  if (typeof obj === "object" && Array.isArray(obj) !== true) {
+    if (typeof obj === "undefined" || typeof obj === "function") {
+      
+    }
+    
+    if (Object.keys(obj)) {
+      var results = [];
+      for (var keys in obj) {
+        if (typeof obj[keys] === "undefined" || typeof obj[keys] === "function") {
+          return '{}';
+        }
+        var property = stringifyJSON(keys) + ':' + stringifyJSON(obj[keys]);
+        results.push(property);
+      }
+      return '{' + results.join(',') + '}';
+    }
+    if (obj !== null) {
+      return '{}'
+    }
+    
+  }
+  
   if (Array.isArray(obj)) {
     if (obj.length !== 0) {
       // for loop (var i etc. etc.)
@@ -53,13 +48,8 @@ var stringifyJSON = function(obj) {
   } else {
     return '' + obj;
   }
-  
-    if (Object.keys(obj).length === 0) {
-    return '{}';
-  }
+
   
 };
-
-// results.push(stringifyJSON(obj[i]));
 
 // return '[' + results.join(',') + ']';
